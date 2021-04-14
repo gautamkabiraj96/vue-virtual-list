1fb1178aa32a09c54c6ecb90c1be7077b8ba166b
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2021-04-15 00:13:49 +0530

    List item component added

---
+++ src/components/ListItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="list-style">{{ index }} - {{ source.title }}</div>
+</template>
+ 
+<script>
+export default {
+  name: "item-component",
+  props: {
+    index: {
+      // index of current item
+      type: Number,
+    },
+    source: {
+      // here is: {uid: 'unique_1', text: 'abc'}
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.list-style {
+  border: 1px solid #eee;
+  background: #eee;
+  padding: 10px 0px;
+  margin: 6px 0;
+}
+</style>
